5b6eda55cbfcf5febffbc7a9a50c52166dc84db9
fbreviglieri <fbrevi.castilho@gmail.com>
2022-06-19 20:55:33 -0300

    Mapper null validaiton

--- Swap.GithubTracker/Swap.GithubTracker.Infra.External/Mappers/GithubTrackPostRequestMapper.cs
+++ Swap.GithubTracker/Swap.GithubTracker.Infra.External/Mappers/GithubTrackPostRequestMapper.cs
@@ -15,7 +15,7 @@ namespace Swap.GithubTracker.Infra.External.Mappers
                 Issues = model.Issues?.Select(x => new IssuePostRequest
                 {
                     Author = x.Author,
-                    Labels = x.Labels.Select(y => y).ToList(),
+                    Labels = x.Labels?.Select(y => y).ToList(),
                     Title = x.Title
                 }).ToList(),
                 Contributors = model.Contributors?.Select(x => new ContributorPostRequest { Name = x.Name, User = x.User, Qtd_Commits = x.CommitsQuantity }).ToList()
